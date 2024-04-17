30978c7784d9d16607ef14addc020a992415e45c
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-04-17 20:19:06 +0530

    created UI

---
+++ 6-Quiz App/script.js
@@ -0,0 +1,52 @@
+const questions = [{
+    'ques': 'What is the correct syntax of doctype in HTML5?',
+    answers: [
+        { 'a': '</doctype html>' },
+        { 'b': '<doctype html>' },
+        { 'c': '<doctype html!>' },
+        { 'd': '<!doctype html>' },
+        { 'correct': 'd' }
+    ]
+},
+{
+    'ques': 'Which of the following tag is used for inserting the largest heading in HTML?',
+    answers: [
+        { 'a': 'head' },
+        { 'b': '<h1>' },
+        { 'c': '<h6>' },
+        { 'd': 'heading' },
+        { 'correct': 'b' }]
+},
+
+{
+    'ques': ' Which element is used for or styling HTML5 layout?',
+    answers: [
+        { 'a': 'CSS' },
+        { 'b': 'JQuery' },
+        { 'c': 'JavaScript' },
+        { 'd': 'PHP' },
+        { 'correct': 'a' }]
+},
+
+{
+    'ques': ' Among the following, which is the HTML paragraph tag?',
+    answers: [
+        { 'a': '<p>' },
+        { 'b': '<pre>' },
+        { 'c': '<hr>' },
+        { 'd': '<a>' },
+        { 'correct': 'a' }]
+},
+
+{
+    'ques': ' Which tag is used to create a numbered list in HTML?',
+    answers: [
+        { 'a': '<ol>' },
+        { 'b': '<ul>' },
+        { 'c': '<li>' },
+        { 'd': 'None Of these' },
+        { 'correct': 'a' }]
+}
+
+
+]
